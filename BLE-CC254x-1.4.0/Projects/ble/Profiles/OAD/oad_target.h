895793787797398957906c7ae5130a07ad69361d
HanShan <hanshan-mail@qq.com>
2017-01-23 16:19:48 +0800

    commit the oad project for master branch

--- BLE-CC254x-1.4.0/Projects/ble/Profiles/OAD/oad_target.h
+++ BLE-CC254x-1.4.0/Projects/ble/Profiles/OAD/oad_target.h
@@ -22,7 +22,7 @@
   its documentation for any purpose.
 
   YOU FURTHER ACKNOWLEDGE AND AGREE THAT THE SOFTWARE AND DOCUMENTATION ARE
-  PROVIDED “AS IS?WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+  PROVIDED “AS IS” WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED,
   INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY, TITLE,
   NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL
   TEXAS INSTRUMENTS OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER CONTRACT,
@@ -50,7 +50,6 @@ extern "C"
 
 #include "hal_aes.h"
 #include "hal_types.h"
-#include "oad.h"
 
 /*********************************************************************
  * CONSTANTS
@@ -81,11 +80,6 @@ extern "C"
 #define OAD_IMG_D_AREA        OAD_IMG_B_AREA
 #define OAD_IMG_R_PAGE        OAD_IMG_A_PAGE
 #define OAD_IMG_R_AREA        OAD_IMG_A_AREA
-#endif
-
-#ifdef OAD_EXT_SUPPORT
-#undef OAD_IMG_R_AREA
-#define OAD_IMG_R_AREA        (128-11)
 #endif
 
 /*********************************************************************
